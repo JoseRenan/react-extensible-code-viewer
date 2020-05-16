9c0bacfe4ef394b0a064295b29138c2a9bdbe70f
José Renan <jose.renan@splab.ufcg.edu.br>
2020-05-15 23:17:57 -0300

    Updates

---
+++ precache-manifest.3f9ddfd181ba7971af6ea532e8ae4593.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5976fffcf4faf654d4c6993e4e6ac462",
+    "url": "/react-extensible-code-viewer/index.html"
+  },
+  {
+    "revision": "907d113026ba7dd5a95f",
+    "url": "/react-extensible-code-viewer/static/css/2.ad8e7604.chunk.css"
+  },
+  {
+    "revision": "ab7148eadfcff7dc1f19",
+    "url": "/react-extensible-code-viewer/static/css/main.4cf2e432.chunk.css"
+  },
+  {
+    "revision": "907d113026ba7dd5a95f",
+    "url": "/react-extensible-code-viewer/static/js/2.56741772.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-extensible-code-viewer/static/js/2.56741772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7148eadfcff7dc1f19",
+    "url": "/react-extensible-code-viewer/static/js/main.dd12c9bc.chunk.js"
+  },
+  {
+    "revision": "5dd4a48ee08c51e2c0b1",
+    "url": "/react-extensible-code-viewer/static/js/runtime-main.8001039b.js"
+  }
+]);
